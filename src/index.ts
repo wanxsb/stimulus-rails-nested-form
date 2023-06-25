bf94250585682840d246aba5f3e1a11abb4598d1
wanxsb <wanxsb@users.noreply.github.com>
2023-06-25 18:07:00 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,24 @@ export default class extends Controller {
   targetTarget: HTMLElement
   templateTarget: HTMLElement
   wrapperSelectorValue: string
+  newRecordExprValue: String
 
   static targets = ['target', 'template']
   static values = {
     wrapperSelector: {
       type: String,
       default: '.nested-form-wrapper'
+    },
+    newRecordExpr: {
+      type: String,
+      default: 'NEW_RECORD'
     }
   }
 
   add (e: Event) {
     e.preventDefault()
 
-    const content: string = this.templateTarget.innerHTML.replace(/NEW_RECORD/, new Date().getTime().toString())
+    const content: string = this.templateTarget.innerHTML.replace(/#{this.newRecordExprValue}/g, new Date().getTime().toString())
     this.targetTarget.insertAdjacentHTML('beforebegin', content)
   }
 
